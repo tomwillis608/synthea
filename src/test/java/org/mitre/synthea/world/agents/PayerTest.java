86ae6235104d30e4433d8e30513851f71a8e3591
hadleynet <marc@hadleynet.org>
2019-12-18 15:09:12 -0500

    Reset provider finder in Provider.clear. Reinstate Claim test

--- src/test/java/org/mitre/synthea/world/agents/PayerTest.java
+++ src/test/java/org/mitre/synthea/world/agents/PayerTest.java
@@ -448,7 +448,7 @@ public class PayerTest {
     assertEquals(totalCost, person.getHealthcareExpenses(), 0.001);
   }
 
-  @Ignore @Test(expected = RuntimeException.class)
+  @Test(expected = RuntimeException.class)
   public void determineCoveredCostWithNullPayer() {
 
     person = new Person(0L);
